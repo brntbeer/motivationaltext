c35c9f980e2e7375f4edc781bdb796e5662d0b43
brntbeer <brent.beer@gmail.com>
2011-03-09 00:46:34 -0500

    added dates field, and number field to form view

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,16 @@ class User
          :recoverable, :rememberable, :trackable, :validatable
 
   field :username
+  field :number
+  field :dates, :type => Array
   validates_presence_of :username
+  validates_presence_of :number
+  validates_length_of :number, :is =>10
   validates_uniqueness_of :username, :email, :case_sensitive => false
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+
+  def before_validation_on_create
+    self.phone = phone.gsub(/[^0-9]/, "")
+  end
 
 end
